ef4c314f1217ba526b859ffa005a67e9f1330a80
Attila Szita <atee88@gmail.com>
2017-10-30 15:12:54 +0100

    Initial commit

---
+++ sptfTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sptfTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
